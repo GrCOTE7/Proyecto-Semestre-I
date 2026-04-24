42d66105bc9f7aae89ff01155e4a55f6e8be9bc4
Axel Quintero <aquintero_2030im@alumni.uma.edu.ve>
2026-04-24 06:24:37 -0400

    Add area data for better section handling in terminal

--- casino/games/__init__.py
+++ casino/games/__init__.py
@@ -1,5 +1,18 @@
 from casino.player import Player
-from utils.terminal import draw_bg, term
+from utils.terminal import draw_bg, term, BG_COLOR
+from dataclasses import dataclass
+
+
+@dataclass
+class Area:
+    """
+    Represents a rectangular area on the terminal screen, defined by its top-left corner (x, y) and its width and height.
+    """
+
+    x: int
+    y: int
+    width: int
+    height: int
 
 
 class Game:
@@ -7,9 +20,31 @@ class Game:
     A base class for all casino games, providing common functionality and structure.
     """
 
+    header_area: Area
+    game_area: Area
+    hint_area: Area
+
+    def __init__(self, name):
+        self.name = name
+        self.header_area = Area(0, 0, term.width, 3)
+        self.game_area = Area(0, 3, term.width, term.height - 6)
+        self.hint_area = Area(0, term.height - 3, term.width, 3)
+
     def start(self):
         print(term.clear())
         draw_bg()
+
+        with term.location(self.header_area.x, self.header_area.y):
+            print(term.move_down(self.header_area.height - 1), end="")
+            print(
+                BG_COLOR
+                + term.center(
+                    term.bold(self.name),
+                    self.header_area.width,
+                    fillchar=BG_COLOR + "-",
+                ),
+                end="",
+            )
 
     def run(self):
         raise NotImplementedError("Subclasses must implement the run() method.")
@@ -18,6 +53,18 @@ class Game:
 
     def show_rules(self):
         raise NotImplementedError("Subclasses must implement the show_rules() method.")
+
+    def show_hint(self, prompt: str):
+        with term.location(self.hint_area.x, self.hint_area.y):
+            print(BG_COLOR + "-" * self.hint_area.width, end="")
+            print(BG_COLOR + term.center(prompt, self.hint_area.width), end="")
+
+    def clear_area(area: Area):
+        """Clears the specified area on the terminal screen by printing spaces over it."""
+        with term.location(area.x, area.y):
+            for _ in range(area.height):
+                print(BG_COLOR + " " * area.width, end="")
+                print(term.move_down, end="")
 
 
 class MultiplayerGame(Game):
